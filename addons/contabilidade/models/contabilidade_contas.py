66d66e0ab434d83cb8e7c4ab1c95f2ff5b5eae95
Mariana Pereira <marianapls2003@gmail.com>
2025-09-24 19:06:59 +0000

    :sparkles: add lógica no código de contas

--- addons/contabilidade/models/contabilidade_contas.py
+++ addons/contabilidade/models/contabilidade_contas.py
@@ -6,7 +6,7 @@ class ContabilidadeContas(models.Model):
 
     name = fields.Char(string="Conta + Código", required=True)
     conta = fields.Char(string="Conta", required=True)
-    codigo = fields.Char(string="Código", required=True)
+    codigo = fields.Char(string="Código", readonly=True)
     descricao = fields.Text(string="Descrição")
 
     grupo_contabil = fields.Selection([
@@ -21,6 +21,7 @@ class ContabilidadeContas(models.Model):
         
     ], string="Grupo Contábil", required=True)
 
+    # TODO: Modificar no campo form para ficar escrito Subgrupo.
     subgrupo1 = fields.Selection([
         ('realizavel', 'Realizável a longo Prazo'),
         ('investimentos', 'Investimentos'),
@@ -29,13 +30,75 @@ class ContabilidadeContas(models.Model):
     ], string="Subgrupo 1")
 
 
-    # TODO: Incluir a lógica de escrever o código automaticamente
-
+    # Lógica de escrever o código automaticamente
     def create(self, vals):
-        vals['name'] = f"{vals.get('codigo')} - {vals.get('conta')}"
+        if not vals.get('codigo') and vals.get('grupo_contabil'):
+            prefixos = {
+                'circulante': '1',
+                'nao_circulante': '2',
+                'passivo_circulante': '3',
+                'passivo_nao_circulante': '4',
+                'patrimonio': '5',
+                'despesa': '6',
+                'apuracao': '6',
+                'receitas': '7',
+            }
+            prefixo = prefixos[vals['grupo_contabil']]
+
+            subgrupo_map = {
+                'realizavel': '0',
+                'investimentos': '1',
+                'imobilizado': '2',
+                'intangivel': '3',
+            }
+
+            if vals['grupo_contabil'] == 'nao_circulante':
+                subgrupo1 = vals.get('subgrupo1')
+                if not subgrupo1:
+                    raise ValueError("Selecione um subgrupo para Ativo Não Circulante.")
+                digito_meio = subgrupo_map[subgrupo]
+
+                ultima = self.search([
+                    ('grupo_contabil', '=', 'nao_circulante'),
+                    ('subgrupo1', '=', subgrupo1)
+                ], order='codigo desc', limit=1)
+
+                if ultima and ultima.codigo:
+                    partes = ultima.codigo.split('.')
+                    partes[-1] = str(int(partes[-1]) + 1)
+                    novo_codigo = '.'.join(partes)
+                else:
+                    novo_codigo = f"{prefixo}.{digito_meio}.1"
+
+            else:
+                ultima = self.search([('grupo_contabil', '=', vals['grupo_contabil'])], order='codigo desc', limit=1)
+                if ultima and ultima.codigo:
+                    partes = ultima.codigo.split('.')
+                    partes[-1] = str(int(partes[-1]) + 1)
+                    novo_codigo = '.'.join(partes)
+                else:
+                    novo_codigo = f"{prefixo}.0.1"
+
+            vals['codigo'] = novo_codigo
+
+        conta_nome = vals.get('conta', '')
+        codigo_val = vals.get('codigo', '')
+        vals['name'] = f"{codigo_val} - {conta_nome}" if conta_nome else codigo_val
+
         return super(ContabilidadeContas, self).create(vals)
-    
+
     def write(self, vals):
         if 'codigo' in vals or 'conta' in vals:
-            vals['name'] = f"{vals.get('codigo')} - {vals.get('conta')}"
+            conta_nome = vals.get('conta', self.conta)
+            codigo_val = vals.get('codigo', self.codigo)
+            vals['name'] = f"{codigo_val} - {conta_nome}" if conta_nome else codigo_val
         return super(ContabilidadeContas, self).write(vals)
+
+    # def create(self, vals):
+    #     vals['name'] = f"{vals.get('codigo')} - {vals.get('conta')}"
+    #     return super(ContabilidadeContas, self).create(vals)
+    
+    # def write(self, vals):
+    #     if 'codigo' in vals or 'conta' in vals:
+    #         vals['name'] = f"{vals.get('codigo')} - {vals.get('conta')}"
+    #     return super(ContabilidadeContas, self).write(vals)
